7ddb07eefd6817e0b101a5edc7832ec8ed0251ee
Josh <joshkpete@gmail.com>
2013-12-03 10:09:59 -0800

    Initial commit.

---
+++ public_library/public_library.py
@@ -0,0 +1,17 @@
+import pdb 
+
+class library(list): 
+	def __init__(self):
+		pass 
+		#library[0] = shelf
+
+	def num_shelves(self): 
+		return len(self)
+
+class shelf(list):
+	pass
+
+class book(list):
+	pass
+
+pdb.set_trace();
