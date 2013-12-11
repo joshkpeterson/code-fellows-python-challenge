6b1d03adbc71ff87de3dccd844f4038f0d7a73a9
Josh <joshkpete@gmail.com>
2013-12-11 04:31:25 -0800

    Add book.unshelf() and comments.

--- public_library/public_library.py
+++ public_library/public_library.py
@@ -1,3 +1,4 @@
+# Requires that you run it either with python -i publiclibrary.py or with pdb
 import pdb 
 
 def main():
@@ -5,11 +6,12 @@ def main():
 	print 'Welcome to the virtual Library-o-matic.'
 	shelves = input('How man shelves would you like: ')
 	my_library = library(shelves)
-	print 'Ok, here\'s a library with ' + str(shelves) + ' shelves.'
+	print 'Ok, here\'s a library called my_library with ' + str(shelves) + ' shelves.'
 
-	book('foo', 2)
-	book('goo', 2)	
-	book('poo', 2)
+	# For debug
+	# book('foo', 2)
+	# book('goo', 2)	
+	# book('poo', 2)
 
 class library(list): 
 	def __init__(self, shelves):
@@ -31,29 +33,70 @@ class shelf(list):
 
 
 	def list_books(self):
-		# if not self:
-		# 	print "Shelf is empty" 
+		if not self:
+			print "Shelf is empty." 
 		for book in self: 
-			print book
+			print book.book_name
 
 class book():
 	current_shelf = None
+	book_name = ''
 
 	def __init__(self, book_name, shelf):
-		name = book_name
+		self.book_name = book_name
+		self.enshelf(book_name, shelf)
+		
+	def _enshelf(self, book_name, shelf):
 		num_shelves = my_library.num_shelves()
 		while ( shelf > (num_shelves - 1) ):
 			print 'The library only has ' + str(num_shelves) + ' shelves.'
 			shelf = input('Pick a different shelf: ')
-		self.enshelf(name, shelf)
-			
-	def enshelf(self, book_name, shelf):
-		my_library[shelf].append(book_name)
-		current_shelf = shelf;
-		print book_name + ' is currently on shelf ' + str(current_shelf) + '.' 
+		my_library[shelf].append(self)
+		self.current_shelf = shelf;
+		print book_name + ' is currently on shelf ' + str(self.current_shelf) + '.' 
 		
+	def unshelf(self):
+		my_library[self.current_shelf].remove(self)
+		shelf = input('What would you like the new shelf to be?')
+		self.enshelf(self.book_name, shelf)
 
-	# def unshelf(self, book_name,)
 main()
 
-pdb.set_trace();
+# Commands:
+# 
+# Your library can be accessed using the variable my_library. 
+# 
+# Number of shelves is taken via user input at runtime.
+# 
+# Add a book:
+# book('book name', shelf)
+# moby_dick = book('Moby Dick', 0)
+# 
+# Print how many shelves in the library:
+# my_library.num_shelves()
+# 
+# List all the books in the library: 
+# my_library.list_books()
+# 
+# List all the books in a single shelf: 
+# my_library[2].list_books()
+# 
+# Move the book: 
+# my_library[0][0].unshelf()
+# moby_dick.unshelf()
+# 
+# CAVEATS:
+# 0-indexing for the shelves is confusing; this could be handled more nicely for the user. 
+# 
+# When you unshelf, you actually create an identical new book object. 
+# What happens to the old one?
+# 
+# In this library, you must enshelf after you unshelf. You don't just go laying books around.
+# This is to prevent a potential 'bug' where a copy of a book could be made by enshelving it again.
+# 
+# Requires that you run it either with python -i publiclibrary.py or with pdb.
+# This is because of the way I rely on global my_library. 
+# I don't know how to conveniently refer to the list that an object is in. 
+
+
+# pdb.set_trace();
